7d7dad00c8b6c6665a9c26c922c5db8c631cf55f
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-11-22 13:10:27 +0200

    Implement the second task

---
+++ docs/lab6/scripts/quiz.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const quizQuestions = [
+    {
+        'question': 'У Мистецтва батько артист?',
+        'answer': 'так',
+    },
+    {
+        'question': 'Торгандроч дзвонить Артуру?',
+        'answer': 'ні',
+    },
+    {
+        'question': 'Інтернет - це пауза?',
+        'answer': 'так',
+    },
+    {
+        'question': 'Торгандроч винен Архімеду гроші?',
+        'answer': 'так',
+    },
+    {
+        'question': 'Нір-Гюль приїхав у Єгипет?',
+        'answer': 'ні',
+    },
+]
+
+const prepareQuiz = () => {
+    alert('Відповідайте так або ні. Починаємо через 5 секунд');
+    setTimeout(startQuiz, 5000)
+}
+
+const startQuiz = () => {
+    let [trueAnswers, falseAnswers] = [0, 0];
+    for (const element of quizQuestions) {
+        const answer = prompt(element.question)
+        if (answer.toLowerCase() === element.answer) trueAnswers++;
+        else falseAnswers++;
+    }
+    alert(`Ви відповіли правильно на ${trueAnswers} питання(ь)\nВи відповіли неправильно на ${falseAnswers} питання(ь)`);
+}
