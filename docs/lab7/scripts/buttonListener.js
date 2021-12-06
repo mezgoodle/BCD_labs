23b27934712f14e92fe6063a507e46cda21f4cdc
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-12-06 19:34:49 +0200

    Do the third task

--- docs/lab7/scripts/buttonListener.js
+++ docs/lab7/scripts/buttonListener.js
@@ -1,5 +1,19 @@
 "use strict";
 
+class Hidden {
+  image = document.getElementById('item-image');
+  handleEvent(event) {
+    switch (this.image.hidden) {
+      case true:
+        alert(`Ви здійснили метод "${event.type}" на об\'єкті ${event.currentTarget}. Тепер картинку не видно.`);
+        break;
+      case false:
+        alert(`Ви здійснили метод "${event.type}" на об\'єкті ${event.currentTarget}. Тепер картинку видно.`);
+        break;
+    }
+  }
+}
+
 const showInfo = () => {
   alert('Найпопулярніші предмети - це граблі та кайло');
 }
